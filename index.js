b9be5eaffa9052fd4a4482a8513710e5430702f3
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-03-18 17:35:28 +0200

    .

--- index.js
+++ index.js
@@ -69,6 +69,9 @@ document.addEventListener("DOMContentLoaded", () => {
       onEnter: () => {
         tm.play();
       },
+      onEnterBack:()=>{
+        tm.progress(100);
+      }
     });
 
     //Scroll Trigger for Out
