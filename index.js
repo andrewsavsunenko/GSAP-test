f8af6705b448e35b38b968aa98a8d2ebf3d0fbae
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-03-22 12:35:54 +0200

    .

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ window.addEventListener("load", () => {
       },{
         yPercent: 0,
         duration: 0.68,
-        delay: 2.6,
+        delay: 2.4,
         ease: "power3.out", 
         stagger: {
           amount: 0.32,
