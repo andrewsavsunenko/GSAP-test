f53d51ab057fe0207edf82caa5880229144bc4a6
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-03-22 14:31:33 +0200

    .

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document
     .querySelectorAll("[text-split] span.word")
     .forEach(function (item) {
-      let c = item.getHTML();
+      let c = item.innerHTML();
       item.innerHTML =
         "<span style='display:block; margin-right:1px;'>" + c + "</span>";
     }); 
