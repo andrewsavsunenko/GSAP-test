949b64301aef2c91df0576e7ff845b163ba2ad74
Andrew Savsunenko <andrew.savsunenko@icloud.com>
2025-03-22 22:53:03 +0200

    .

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ class App {
           yPercent: 0,
           duration: 0.8,
           delay: 2.9,
-          ease: "power1.out",
+          ease: "power2.out",
           stagger: { amount: 0.32 },
         }
       );
